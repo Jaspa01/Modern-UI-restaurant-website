8414f9acb042e481194e3484b60259ca82c88c4b
Jasper <adindechybke@gmail.com>
2023-10-04 19:21:01 +0100

    Updated AboutUs

--- modern-ui-restaurant/src/container/AboutUs/AboutUs.jsx
+++ modern-ui-restaurant/src/container/AboutUs/AboutUs.jsx
@@ -1,11 +1,48 @@
-import React from 'react';
+import React from "react";
 
-import './AboutUs.css';
+import "./AboutUs.css";
+import images from "../../constants/images";
 
-const AboutUs = () => (
-  <div>
-    AboutUs
-  </div>
-);
+const AboutUs = () =>
+  <div
+    className="app__aboutus app__bg flex__center section__padding"
+    id="about"
+  >
+    <div className="app__aboutus-overlay flex__center">
+      <img src={images.G} alt="g letter" />
+    </div>
+
+    <div className="app__aboutus-content flex__center">
+      <div className="app__aboutus-content_about">
+        <h1 className="headtext__cormorant">About Us</h1>
+        <img src={images.spoon} alt="about_spoon" />
+        <p className="p__opensans">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra
+          adipiscing ultrices vulputate posuere tristique. In sed odio nec
+          aliquet eu proin mauris et.
+        </p>
+        <button className="custom__button" style={{ marginTop: "1.5rem" }}>
+          Know More
+        </button>
+      </div>
+
+      <div className="app__aboutus-content_knife">
+        <img src={images.knife} alt="knife img" />
+      </div>
+
+      <div className="app__aboutus-content_history">
+        <h1 className="headtext__cormorant">Our History</h1>
+        <img src={images.spoon} alt="about_spoon" />
+        <p className="p__opensans">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra
+          adipiscing ultrices vulputate posuere tristique. In sed odio nec
+          aliquet eu proin mauris et.
+        </p>
+        <button className="custom__button" style={{ marginTop: "1.5rem" }}>
+          Know More
+        </button>
+      </div>
+    </div>
+  </div>;
 
 export default AboutUs;
