11be8a5daf2fae2a64963b2e474fa4b9a6db722f
Jasper <adindechybke@gmail.com>
2023-10-04 18:23:22 +0100

    Updated Header

--- modern-ui-restaurant/src/container/Header/Header.jsx
+++ modern-ui-restaurant/src/container/Header/Header.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
 import './Header.css';
+import images from '../../constants/images'
+import { SubHeading } from '../../components';
 
 const Header = () => (
-  <div>
-    Header
+  <div className='app__header app__wrapper section__padding' id="home">
+    <div className='app__wrapper_info'>
+      <SubHeading title="Chase the new flavour" />
+      <h1 className='app__header-h1'>The key to Fine Dining</h1>
+      <p className="p__opensans" style={{margin: "0, 2rem"}}>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus</p>
+      <button className='custom__button' style={{marginTop: "1.5rem"}}>Explore Menu</button>
+    </div>
+    <div className='app__wrapper_img'>
+      <img src={images.welcome} alt="" />
+    </div>
   </div>
 );
 
