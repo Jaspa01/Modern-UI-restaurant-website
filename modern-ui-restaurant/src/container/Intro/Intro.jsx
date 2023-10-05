8d66cd05c997e7b70087da1332b91fae17b7b68f
Jasper <adindechybke@gmail.com>
2023-10-05 02:18:55 +0100

    Updated Intro

--- modern-ui-restaurant/src/container/Intro/Intro.jsx
+++ modern-ui-restaurant/src/container/Intro/Intro.jsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useRef, useState } from "react";
+import { BsPause } from "react-icons/bs";
+import { BiPlay } from "react-icons/bi";
+// BiPlay
 
-import './Intro.css';
+import "./Intro.css";
+import { meal } from "../../constants";
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+const Intro = () => {
+  const [playVideo, setPlayVideo] = useState(false);
+  const vidRef = useRef();
+
+  const handleVideo = () => {
+    setPlayVideo(prevPlayVideo => !prevPlayVideo);
+
+    if (playVideo) {
+      vidRef.current.pause();
+    } else {
+      vidRef.current.play();
+    }
+  };
+
+  return (
+    <div className="app__video">
+      <video src={meal} ref={vidRef} type="video/mp4" loop controls={false} />
+
+      <div className="app__video-overlay flex__center">
+        <div
+          className="app__video-overlay_circle flex__center"
+          onClick={handleVideo}
+        >
+          {playVideo
+            ? <BsPause color="#DCCA87" fontSize={30} />
+            : <BiPlay color="#DCCA87" fontSize={30} />}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Intro;
